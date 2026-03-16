f664696f2068de89bc772a4ff271600120acea16
Shrija Gangapuram <shrija.ganga@gmail.com>
2026-03-16 11:28:55 +0530

    updated login

--- client/src/pages/Loginpage.jsx
+++ client/src/pages/Loginpage.jsx
@@ -9,20 +9,20 @@ function Loginpage() {
   }
 
   return(
-    <div className=" mt-10">
-      <form onSubmit={handleSubmit(onUserLogin)} className="">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 mt-10">
+      <form onSubmit={handleSubmit(onUserLogin)} className="bg-white shadow-lg rounded-lg p-8 w-100">
 
-        <h2 className="">Login</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
 
-        <div className="">
-          <input type="email" placeholder="Email" {...register("email",{required:true})} className=""/>
+        <div className="mb-4">
+          <input type="email" placeholder="Email" {...register("email",{required:true})} className="w-full border rounded p-2 mb-2"/>
         </div>
 
         <div className="">
-          <input type="password" placeholder="Password" {...register("password",{required:true})} className=""/>
+          <input type="password" placeholder="Password" {...register("password",{required:true})} className="w-full border rounded p-2 mb-4"/>
         </div>
 
-        <button className="">Login</button>
+        <button className="w-full mb-4 bg-blue-600 text-white p-2 rounded hover:bg-blue-700">Login</button>
       </form>
     </div>
   )
