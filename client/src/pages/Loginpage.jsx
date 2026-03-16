a3544e964a857ba74813438ed309f5595c970a95
Shrija Gangapuram <shrija.ganga@gmail.com>
2026-03-16 10:48:21 +0530

    login - frontend

---
+++ client/src/pages/Loginpage.jsx
@@ -0,0 +1,54 @@
+import { useForm } from "react-hook-form";
+import { useAuth } from "../store/authStore";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { errorClass, submitBtn, formCard, formTitle, inputClass, formGroup } from "../styles/common";
+import {toast} from 'react-hot-toast'
+
+function Login() {
+
+  const {register,handleSubmit,formState:{errors}}=useForm()
+  const login = useAuth(state => state.login)
+  const isAuthenticated = useAuth((state)=> state.isAuthenticated)
+  const currentUser = useAuth((state)=>state.currentUser)
+  const error = useAuth((state)=>state.error)
+  const navigate = useNavigate()
+
+  const onUserLogin = async(userCredObj) => {
+    await login(userCredObj)
+  }
+
+  useEffect(()=>{
+    if (isAuthenticated){
+      if(currentUser.role === "USER"){
+        toast.success("Logged in successfully")
+        navigate("/userdashboard")
+      }
+    if(currentUser.role === "AUTHOR"){
+      toast.success("Logged in successfully")
+      navigate("/authordashboard")
+    }
+  }},[isAuthenticated,currentUser])
+
+  return(
+    <div className=" mt-10">
+      <form onSubmit={handleSubmit(onUserLogin)} className={formCard}>
+        {error && <p className={errorClass}>{error}</p>}
+
+        <h2 className={formTitle}>Login</h2>
+
+        <div className={formGroup}>
+          <input type="email" placeholder="Email" {...register("email",{required:true})} className={inputClass}/>
+        </div>
+
+        <div className={formGroup}>
+          <input type="password" placeholder="Password" {...register("password",{required:true})} className={inputClass}/>
+        </div>
+
+        <button className={submitBtn}>Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
