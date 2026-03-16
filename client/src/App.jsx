635b43e062ed07b4e0e628aa475fd4fd5fd99ba9
Shrija Gangapuram <shrija.ganga@gmail.com>
2026-03-16 20:25:56 +0530

    update
    user dashboard and board page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
           element:<UserDashboard />
         },
         {
-          path:"board/:id",
+          path:"create-board",
           element:<BoardPage/>
         }
       ]
