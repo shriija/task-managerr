2d1674cad0cef4929ecaf1a07c188c392bf7f50e
Manohar <naniswini123@gmail.com>
2026-03-16 11:13:56 +0530

    Update Loginpage.jsx

--- client/src/pages/Loginpage.jsx
+++ client/src/pages/Loginpage.jsx
@@ -1,38 +1,28 @@
 import { useForm } from "react-hook-form";
-import { useAuth } from "../store/authStore";
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
-import { errorClass, submitBtn, formCard, formTitle, inputClass, formGroup } from "../styles/common";
-import {toast} from 'react-hot-toast'
 
 function Loginpage() {
 
   const {register,handleSubmit,formState:{errors}}=useForm()
-  const login = useAuth(state => state.login)
-  const isAuthenticated = useAuth((state)=> state.isAuthenticated)
-  const currentUser = useAuth((state)=>state.currentUser)
-  const error = useAuth((state)=>state.error)
-
+// 
   const onUserLogin = async(userCredObj) => {
     await login(userCredObj)
   }
 
   return(
     <div className=" mt-10">
-      <form onSubmit={handleSubmit(onUserLogin)} className={formCard}>
-        {error && <p className={errorClass}>{error}</p>}
+      <form onSubmit={handleSubmit(onUserLogin)} className="">
 
-        <h2 className={formTitle}>Login</h2>
+        <h2 className="">Login</h2>
 
-        <div className={formGroup}>
-          <input type="email" placeholder="Email" {...register("email",{required:true})} className={inputClass}/>
+        <div className="">
+          <input type="email" placeholder="Email" {...register("email",{required:true})} className=""/>
         </div>
 
-        <div className={formGroup}>
-          <input type="password" placeholder="Password" {...register("password",{required:true})} className={inputClass}/>
+        <div className="">
+          <input type="password" placeholder="Password" {...register("password",{required:true})} className=""/>
         </div>
 
-        <button className={submitBtn}>Login</button>
+        <button className="">Login</button>
       </form>
     </div>
   )
