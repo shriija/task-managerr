ad74ebbc9f0632100a3361acd945e7e29daeaf66
Devisriprasad <thisisdsp54@gmail.com>
2026-03-16 13:11:57 +0530

    routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,20 +1,63 @@
 import {UserModel} from '../models/User.js'
 import bcrypt from 'bcryptjs'
+import {generateToken} from '../utils/generateToken.js'
+
 export const signup = async(req,res) =>{
     const user = req.body;
+    console.log(user)
     try {
-        const response = await UserModel.validate();
-        const hashedPass = bcrypt.hash(user.pass)
+        await UserModel.validate(user);
+        const hashedPass = await bcrypt.hash(user.password,8)
+        const newUser = new UserModel({
+            name:user.name,
+            email:user.email,
+            password:hashedPass,
+            avatar:user.avatar
+        })
+        newUser.save()
+        res.status(501).json({message:"user created sucessfully"})
     } catch (error) {
-        
+        console.log("error in signup")
+        res.json({error:error.message})
     }
 }
 export const signin = async(req,res)=>{
+    const {name,password} = req.body;
+    try {
+        const user = await UserModel.findOne({name})
+        if(!user){
+            return res.status(404).json({message:"user not found"})
+        }
 
+        const response = await bcrypt.compare(password,user.password)
+        if(!response){
+            return res.status(400).json({message:"invalid credentials"})
+        }
+        const userObj = user.toObject()
+        delete userObj.password
+        const token = generateToken(userObj)
+        
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            secure: false,
+            sameSite: "lax"
+        });
+        res.status(501).json({message:"signin sucessfull",payload:userObj})
+
+    } catch (error) {
+        console.log("error in signin")
+        res.status(401).json({error:error.message})
+    }
 }
 export const logout = async(req,res)=>{
-    
-}
-export const authenticate = async(req,res) =>{
-
+    try {
+        const userId = req.params.id;
+        res.cookie("jwt",'')
+        res.status(501).json({message:"logout sucessfull"});
+        
+    } catch (error) {
+        console.log("error in logout")
+        res.status(401).json({error:error.message})
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import exp from 'express'
 import {config} from 'dotenv'
 import connectDB from './config/db.js'
+import UserApi from './Apis/UserApi.js'
+import CookieParser from 'cookie-parser'
 export const app = exp()
 config()
 app.use(exp.json())
-
+app.use(CookieParser())
+app.use('/user-api',UserApi)
 await connectDB()
 app.listen(process.env.PORT,()=>{
     console.log("server is running on port 4001")
