82a6defdf4ec5a66859118f0f84699e2a4f3361c
Devisriprasad <thisisdsp54@gmail.com>
2026-03-16 15:23:13 +0530

    listModel changes

--- server/controllers/ListController.js
+++ server/controllers/ListController.js
@@ -1,14 +1,18 @@
 import {ListModel} from '../models/List.js'
 
 export const AddList = async(req,res) =>{
+    const listInfo = req.body;
     const {title,board,position} = req.body;
+    console.log(title,board,position)
     try {
-        await ListModel.validate({title:title,board:board})
+        await ListModel.validate(listInfo)
+        console.log("safe 1")
         const list = new ListModel({
             title:title,
             board:board,
             position:position
         })
+        console.log("safe2")
         list.save()
         res.status(201).json({message:"list created",payload:list})
     } catch (error) {
