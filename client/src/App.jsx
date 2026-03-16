a7e77b3254fc7c2165346c675222197d5ee97f98
Manohar <naniswini123@gmail.com>
2026-03-16 11:11:57 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { createBrowserRouter,RouterProvider } from 'react-router'
 import RegisterPage from './pages/RegisterPage'
+import Loginpage from './pages/Loginpage'
 
 function App() {
   return (
     <>
     <RegisterPage/>
+    <Loginpage />
     </>
   )
 }
